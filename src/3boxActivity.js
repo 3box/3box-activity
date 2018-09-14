9636375bb6f37172804e12d1eda60b91f77ead90
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-09-14 12:08:41 -0300

    add linter to 3box activity

--- src/3boxActivity.js
+++ src/3boxActivity.js
@@ -1,6 +1,6 @@
-const etherscanAPI = require("etherscan-api").init(
-  "T17Z92YN5TTT61T4XT2A85K2KEXICP1XUQ"
-);
+const etherscanAPI = require('etherscan-api').init(
+  'T17Z92YN5TTT61T4XT2A85K2KEXICP1XUQ'
+)
 
 class ThreeBoxActivity {
   /**
@@ -13,36 +13,36 @@ class ThreeBoxActivity {
    *
    */
 
-  static async get(address) {
-    if (!address) throw new Error("no address");
-    let internal = { result: [] };
-    let txs = { result: [] };
-    let tokens = { result: [] };
+  static async get (address) {
+    if (!address) throw new Error('no address')
+    let internal = { result: [] }
+    let txs = { result: [] }
+    let tokens = { result: [] }
 
     try {
-      internal = await etherscanAPI.account.txlistinternal(null, address);
+      internal = await etherscanAPI.account.txlistinternal(null, address)
     } catch (err) {
-      console.log("no internal txs");
+      console.log('no internal txs')
     }
 
     try {
-      txs = await etherscanAPI.account.txlist(address);
+      txs = await etherscanAPI.account.txlist(address)
     } catch (err) {
-      console.log("no transactions");
+      console.log('no transactions')
     }
 
     try {
-      tokens = await etherscanAPI.account.tokentx(address);
+      tokens = await etherscanAPI.account.tokentx(address)
     } catch (err) {
-      console.log("no token transfers");
+      console.log('no token transfers')
     }
 
     return {
       txs: txs.result,
       internal: internal.result,
       token: tokens.result
-    };
+    }
   }
 }
 
-module.exports = ThreeBoxActivity;
+module.exports = ThreeBoxActivity
